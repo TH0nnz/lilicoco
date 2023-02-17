4e0f17f40697f7a240ada0e715c6fcb4245ee37b
tom <jheng.tom@gmail.com>
2023-02-17 23:39:32 +0800

    init

---
+++ src/main/java/com/tomjheng/lilicoco/LilicocoApplication.java
@@ -0,0 +1,13 @@
+package com.tomjheng.lilicoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LilicocoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LilicocoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tomjheng/lilicoco/LilicocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomjheng.lilicoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LilicocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
